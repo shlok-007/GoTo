7e649595a2b6d605199591754bcda52c9f336ea6
Shlok Kumar Shaw <shlok845@gmail.com>
2023-06-12 20:02:51 +0530

    Added Login Page and configured it routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+
 import {useGoogleOneTapLogin, googleLogout} from '@react-oauth/google';
 import decodeJwtResponse from './utils/decodeJwtResponse';
+
 import Navbar from './components/Navbar';
-import LoggedInPage from './Pages/LoggedInPage';
+import DestinationSelect from './Pages/DestinationSelect';
+import LoginPage from './Pages/LoginPage';
+
 import profile_interface from './types/profile_interface';
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, useNavigate} from 'react-router-dom';
 
 const App: React.FC = () => {
 
   const [isLogged, setIsLogged] = useState<boolean>(false);
-  const [credential, setCredential] = useState<string>("");
   const [profile, setProfile] = useState<profile_interface | undefined>(undefined);
+  const navigate = useNavigate();
 
-  useGoogleOneTapLogin({
-    onSuccess: credentialResponse => {
-      setIsLogged(true);
-      if(credentialResponse.credential){ 
-        setCredential(credentialResponse.credential);
-        setProfile(decodeJwtResponse(credentialResponse.credential));
-      }
-    },
-    onError: () => {
-      console.log('Login Failed');
-    },
-    cancel_on_tap_outside: false,
-    // prompt_parent_id: 'google-one-tap-button',
-  });
+  const CallLogin = ()=>{
+    useGoogleOneTapLogin({
+      onSuccess: credentialResponse => {
+        setIsLogged(true);
+        if(credentialResponse.credential){ 
+          setProfile(decodeJwtResponse(credentialResponse.credential));
+        }
+        navigate("/selectDestination");
+      },
+      onError: () => {
+        console.log('Login Failed');
+      },
+      cancel_on_tap_outside: false,
+      prompt_parent_id: 'google-one-tap-button',
+    });
+  };
 
   const handleLogout = () => {
     setIsLogged(false);
-    setCredential("");
     setProfile(undefined);
     googleLogout();
+    navigate("/");
   };
-
-
-  // const gotoDB = client.db("GoTo");
-  // const gotoCollection = gotoDB.collection("GoToUsers");
-  // const guser = gotoCollection.findOne();
-  // console.log(guser);
 
   return (
     <>
 
       <Navbar isLogged={isLogged} profile={profile} siteName="GoTogether" onLogout={handleLogout}/>
 
-
-      {/* for testing */}
-
-      <LoggedInPage profile={profile}/>
-
+      <Routes>
+        <Route path="/" element={<LoginPage loginPromptFunction={CallLogin}/>}/>
+        <Route path="/selectDestination" element={<DestinationSelect profile={profile}/>}/>
+      </Routes>
     </>
   );
 }
