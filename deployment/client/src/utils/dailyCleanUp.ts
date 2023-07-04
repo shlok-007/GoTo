684752e7dffe4331f087198d3a6b54c959cbbae9
Shlok Kumar Shaw <shlok845@gmail.com>
2023-07-05 01:18:13 +0530

    Fixing dailyCleanup

--- deployment/client/src/utils/dailyCleanUp.ts
+++ deployment/client/src/utils/dailyCleanUp.ts
@@ -2,9 +2,11 @@ export default async function dailyCleanUp() {
     try{
         console.log("dailyCleanUp");
         let serverURL = process.env.REACT_APP_SERVER_URL;
-        await fetch(serverURL+"/travelDetails/dailyCleanUp",{
+        const response = await fetch(serverURL+"/travelDetails/dailyCleanUp",{
             method: 'DELETE'
         });
+        const data = await response.json();
+        console.log(data);
     }catch(err){
         console.log(err);
     }
