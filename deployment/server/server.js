ade86e09a2506fcc40db7915f823067759f7fa7f
Shlok Kumar Shaw <shlok845@gmail.com>
2023-07-05 13:40:57 +0530

    Fixing deployment issues

--- deployment/server/server.js
+++ deployment/server/server.js
@@ -24,6 +24,6 @@ app.get('*',(req,res,next)=>{
 // export default app;
 
 // start the Express server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port: ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
