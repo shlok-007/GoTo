3527f94fe12e53fd90b942e07092084965827820
Shlok Kumar Shaw <shlok845@gmail.com>
2024-05-11 03:40:38 +0530

    Added rate limiting for backend requests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "./loadEnvironment.js";
 import "./cronDailyCleanup.js"
+import rateLimit from "express-rate-limit";
+import jwt from 'jsonwebtoken';
 
 import travelDetailsCollection from "./routes/travelDetailsCollection.js";
 import getDateTime from "./routes/getDateTime.js"
@@ -18,6 +20,25 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+const limiter = rateLimit({
+  windowMs: 1*60*1000,
+  max: 30,
+  keyGenerator: (req) => {
+    const authToken = req.cookies.jwt_auth_token;
+    if (authToken) {
+      try {
+        const user = jwt.verify(authToken, process.env.JWT_SECRET);
+        return user.email;
+      } catch (err) {
+        return req.ip;
+      }
+    }
+    return req.ip;
+  }
+});
+
+app.use(limiter);
 
 app.use("/travelDetails", verifyJWT, travelDetailsCollection);
 app.use("/getDateTime", verifyJWT, getDateTime);
