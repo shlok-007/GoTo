e37f3c635281b93542a0b0531612ad55c4e00db7
VARSHA SWARAJ <126698394+callmeVAMP@users.noreply.github.com>
2024-05-12 20:44:56 +0530

    Update Footer.tsx
    updated footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Footer.css';
 
+
+
 function Footer() {
   return (
     <footer>
@@ -8,14 +10,23 @@ function Footer() {
         <div className="footer-row">
           <div className="footer-col">
             <h4>About Us</h4>
-            <p>Welcome to our logistics company Vitaran! We are a leading provider of comprehensive logistics solutions tailored to meet the diverse needs of our clients. With a deep understanding of the logistics industry and a commitment to excellence, we strive to deliver efficient and reliable services that streamline supply chains and drive business success.</p>
-          </div>
+            <p>Welcome to our logistics company GoTo!<br></br>We are here to help you.</p>
+            
+          </div> 
           <div className="footer-col">
             <h4>Contact Us</h4>
             <ul>
               <li>Email: info@vitaran.com</li>
               <li>Phone: +91 99129-85659</li>
-              <li>Address: 123, Andheri East, Navi Mumbai</li>
+              
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Follow Us</h4>
+            <ul>
+              <li>Insta: </li>
+              
+              <li>Facebook: </li>
             </ul>
           </div>
 
