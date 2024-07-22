a2c78ca95819c0be3c5b10535b548dec1b29bcba
Shlok Kumar Shaw <shlok845@gmail.com>
2024-07-22 12:19:22 +0530

    Added dynamic reloading of jwk.json file for OAuth verification

--- server/verifyOAuthJWT.js
+++ server/verifyOAuthJWT.js
@@ -4,14 +4,26 @@ import { fetchOAuthJWKs } from "./cronFetchOAuthJWKs.js";
 
 let jwk = {};
 
-try{
-    jwk = JSON.parse(fs.readFileSync("./jwk.json", "utf8"));
-}catch(e){
-    console.log("Error reading jwk.json");
-    // console.log(e);
-    await fetchOAuthJWKs();
-    jwk = JSON.parse(fs.readFileSync("./jwk.json", "utf8"));
-}
+const readJWKFile = async () => {
+    try{
+        jwk = JSON.parse(fs.readFileSync("./jwk.json", "utf8"));
+    }catch(e){
+        console.log("Error reading jwk.json");
+        // console.log(e);
+        await fetchOAuthJWKs();
+        // jwk = JSON.parse(fs.readFileSync("./jwk.json", "utf8"));
+    }
+};
+
+await readJWKFile();
+
+fs.watch("./jwk.json", async (eventType, filename) => {
+    if (eventType === "change") {
+        console.log("jwk.json file has been updated. Reloading...");
+        await readJWKFile();
+    }
+});
+
 
 const verifyOAuthJWT = async (oauthJWT) => {
     const body = JSON.parse(Buffer.from(oauthJWT.split(".")[1], "base64").toString());
@@ -22,18 +34,26 @@ const verifyOAuthJWT = async (oauthJWT) => {
     // console.log(jws.verify(oauthJWT, jwk));
 
     if( body.aud === process.env.OAUTH_CLIENT_ID && 
-        body.iss === 'https://accounts.google.com' && 
-        jws.verify(oauthJWT, jwk) ){
-
-        // console.log("JWT verified");
-        let userData = {
-            email: body.email,
-            name: body.name,
-            picture: body.picture,
-            sub: body.sub,
-            given_name: body.given_name
-        };
-        return userData;
+        body.iss === 'https://accounts.google.com' 
+        // && jws.verify(oauthJWT, jwk) 
+    ){
+        try{
+            jws.verify(oauthJWT, jwk);
+            // console.log("JWT verified");
+            let userData = {
+                email: body.email,
+                name: body.name,
+                picture: body.picture,
+                sub: body.sub,
+                given_name: body.given_name
+            };
+            return userData;
+        }
+        catch(e){
+            console.log("JWT verification failed");
+            await fetchOAuthJWKs();
+        }
+        
     }
         
     return false;
